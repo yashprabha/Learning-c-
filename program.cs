edfe9b264571db5a751bca32c1df67d793e601bf
yashprabha <yashprabha07@gmail.com>
2020-05-01 19:12:32 +0100

    initial comment

---
+++ program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyYash1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  Learning about Console Writeline
+           //  Console.WriteLine("Hello World!");
+          //  Console.WriteLine("   /|");
+          //   Console.WriteLine("  / |");
+           // Console.WriteLine(" /  |");
+           // Console.WriteLine("/___|");
+
+           // learning about Variables
+           // String characterName = "Yashu";
+           // int characterAge;
+            //characterAge = 38;
+            //Console.WriteLine("There was a beautiful girl named " + characterName);
+           // Console.WriteLine("She was "+ characterAge +" years old");
+          //  Console.WriteLine(characterName + " loves creating crafts and making programs");
+
+          //  characterName = "Yashi";
+          //  Console.WriteLine(characterName + " achieved success at the age " + characterAge );
+
+            // Learning about Datatypes
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
